f8e86ccb9bd767e00eda38d87c8e231066d40991
Yuval Alaluf <yuvalalaluf@gmail.com>
2021-04-06 19:04:36 +0300

    Add initial files

---
+++ utils/model_utils.py
@@ -0,0 +1,18 @@
+RESNET_MAPPING = {
+    'layer1.0': 'body.0',
+    'layer1.1': 'body.1',
+    'layer1.2': 'body.2',
+    'layer2.0': 'body.3',
+    'layer2.1': 'body.4',
+    'layer2.2': 'body.5',
+    'layer2.3': 'body.6',
+    'layer3.0': 'body.7',
+    'layer3.1': 'body.8',
+    'layer3.2': 'body.9',
+    'layer3.3': 'body.10',
+    'layer3.4': 'body.11',
+    'layer3.5': 'body.12',
+    'layer4.0': 'body.13',
+    'layer4.1': 'body.14',
+    'layer4.2': 'body.15',
+}
